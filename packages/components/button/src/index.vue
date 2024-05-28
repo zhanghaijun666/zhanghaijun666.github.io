2bb95202d8e5fb68f0cd26b24480a1627c7b8fbd
zhanghaijun <zhanghaijun@bjtxra.com>
2024-05-28 19:11:39 +0800

    init

---
+++ packages/components/button/src/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button>
+      <span class="text">测试</span>
+    </button>
+  </div>
+</template>
+<script lang="ts" setup name="MButton"></script>
+<style scoped lang="scss">
+.text {
+  color: green;
+}
+</style>
---
+++ packages/components/index.ts
@@ -0,0 +1,22 @@
+import type { Component, App } from "vue";
+import TButton from "./button";
+
+// 存储组件列表
+const components: { [propName: string]: Component } = {
+  TButton,
+};
+
+// - install：每个插件都有一个 install 方法
+const install: any = (app: any): void => {
+  for (const key in components) {
+    app.component(key, components[key]);
+  }
+};
+
+/**
+ * @description 导出组件
+ */
+export { TButton };
+
+const TuiPlus = { install };
+export default TuiPlus;
