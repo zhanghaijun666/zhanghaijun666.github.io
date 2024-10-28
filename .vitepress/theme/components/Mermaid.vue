087296a58616b1abad1804eb01eb5e4af41eb169
爱做梦的奋斗青年 <zhanghaijun_java@163.com>
2024-10-28 18:41:52 +0800

    feat: mermaid暗黑主题适配
    Signed-off-by: zhanghaijun <zhanghaijun@imeik.com>

--- .vitepress/theme/components/Mermaid.vue
+++ .vitepress/theme/components/Mermaid.vue
@@ -2,15 +2,18 @@
   <div v-html="svgRef" style="display: flex;justify-content: center;align-items: center;"></div>
 </template>
 <script setup lang="ts">
+import { useData } from 'vitepress'
 import { onMounted, ref } from 'vue'
 import mermaid from 'mermaid'
 
-const props = defineProps({ id: String, code: String })
+const props = defineProps({id: String, code: String})
+
+const {isDark} = useData()
 
 const render = async (id: string, code: string) => {
   // mermaid 初始化
-  mermaid.initialize({ startOnLoad: false })
-  const { svg } = await mermaid.render(id, code)
+  mermaid.initialize({startOnLoad: false, theme: isDark.value ? 'dark' : 'default'})
+  const {svg} = await mermaid.render(id, code)
   return svg
 }
 // 在组件挂载后进行mermaid渲染
