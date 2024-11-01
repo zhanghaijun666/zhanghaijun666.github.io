59ee28dd84cc0a90f200e6195a7b23bd527bf65b
zhanghaijun <zhanghaijun@imeik.com>
2024-11-01 11:56:58 +0800

    feat: 新增链接
    Signed-off-by: zhanghaijun <zhanghaijun@imeik.com>

--- .vitepress/data/link.ts
+++ .vitepress/data/link.ts
@@ -21,53 +21,55 @@ export const webLinks: Group[] = [
     title: 'Web',
     icon: 'fab fa-github',
     links: [
-      { name: 'Google', href: 'https://www.google.com' },
-      { name: 'Bing', href: 'https://www.bing.com' },
-      { name: 'GitHub', href: 'https://github.com' },
-      { name: 'Gitee', href: 'https://gitee.com' },
-      { name: '科学上网', href: 'https://ikuuu.pw' }
+      {name: 'Google', href: 'https://www.google.com'},
+      {name: 'Bing', href: 'https://www.bing.com'},
+      {name: 'GitHub', href: 'https://github.com'},
+      {name: 'Gitee', href: 'https://gitee.com'},
+      {name: '科学上网', href: 'https://ikuuu.pw'}
     ]
   },
   {
     title: 'AI',
     icon: 'fa fa-umbrella',
     links: [
-      { name: 'ChatGPT', href: 'https://chatgpt.com/' },
-      { name: '通义千问', href: 'https://tongyi.aliyun.com/' },
-      { name: '文心一言', href: 'https://yiyan.baidu.com/' },
-      { name: '豆包', href: 'https://www.doubao.com/' },
-      { name: 'KiMi', href: 'https://kimi.moonshot.cn/' },
-      { name: '图灵', href: 'https://www.tuling123.com/' },
-      { name: '天工 AI', href: 'https://www.tiangong.cn/' }
+      {name: 'ChatGPT', href: 'https://chatgpt.com/'},
+      {name: '通义千问', href: 'https://tongyi.aliyun.com/'},
+      {name: '文心一言', href: 'https://yiyan.baidu.com/'},
+      {name: '豆包', href: 'https://www.doubao.com/'},
+      {name: 'KiMi', href: 'https://kimi.moonshot.cn/'},
+      {name: '图灵', href: 'https://www.tuling123.com/'},
+      {name: '天工 AI', href: 'https://www.tiangong.cn/'}
     ]
   },
   {
     title: '开发工具',
     icon: 'fa fa-handshake',
     links: [
-      { name: 'JetBrains-激活码', href: 'https://3.jetbra.in/' },
-      { name: '阿里云效', href: 'https://devops.aliyun.com/' },
-      { name: '在线工具', href: 'https://it-tools.tech/' },
-      { name: '微信开发文档', href: 'https://developers.weixin.qq.com/doc/' },
-      { name: 'Linux命令查询', href: 'https://command-not-found.com/' },
-      { name: 'KubeSphere', href: 'https://kubesphere.io/zh/' },
-      { name: 'Helm K8S', href: 'https://helm.sh/zh/' }
+      {name: 'JetBrains-激活码', href: 'https://3.jetbra.in/'},
+      {name: '阿里云效', href: 'https://devops.aliyun.com/'},
+      {name: '在线工具', href: 'https://it-tools.tech/'},
+      {name: '微信开发文档', href: 'https://developers.weixin.qq.com/doc/'},
+      {name: 'Linux命令查询', href: 'https://command-not-found.com/'},
+      {name: 'KubeSphere', href: 'https://kubesphere.io/zh/'},
+      {name: 'Helm K8S', href: 'https://helm.sh/zh/'}
     ]
   },
   {
     title: 'WEB开发',
     icon: 'fa fa-window-restore',
     links: [
-      { name: 'knockoutJS', href: 'https://knockoutjs.com/downloads/index.html' },
-      { name: 'ElementPlus', href: 'https://element-plus.org/zh-CN' }
+      {name: 'knockoutJS', href: 'https://knockoutjs.com/downloads/index.html'},
+      {name: 'ElementPlus', href: 'https://element-plus.org/zh-CN'},
+      {name: "UnoCSS", href: "https://unocss.net/"},
+      {name: "Animate", href: "https://www.dowebok.com/demo/2014/98/"}
     ]
   },
   {
     title: '其他',
     icon: 'fa fa-link',
     links: [
-      { name: '易搜', href: 'https://yiso.fun' },
-      { name: '易搜地址', href: 'https://yiso.eu.org/' }
+      {name: '易搜', href: 'https://yiso.fun'},
+      {name: '易搜地址', href: 'https://yiso.eu.org/'}
     ]
   }
 ]
